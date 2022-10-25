850a8d3d1d2a6dc8f8158e4ab7b9cda47e8222ff
sseoyeong <ssss0012ss@naver.com>
2022-10-25 22:18:49 +0900

    test 페이지

---
+++ src/main/java/com/hotel/josunhotel20221025seoyeong/Josunhotel20221025SeoyeongApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.josunhotel20221025seoyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Josunhotel20221025SeoyeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Josunhotel20221025SeoyeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/josunhotel20221025seoyeong/Josunhotel20221025SeoyeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.josunhotel20221025seoyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Josunhotel20221025SeoyeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
