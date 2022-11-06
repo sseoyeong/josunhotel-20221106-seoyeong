f4717a1d6fa2e4310beb29b9e2945aee8b52f188
sseoyeong <ssss0012ss@naver.com>
2022-11-07 01:32:42 +0900

    로그인창 구현

---
+++ src/main/java/com/hotel/josunhotel20221025seoyeong/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.hotel.josunhotel20221025seoyeong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    @GetMapping("/account/login")
+    public String login() {
+        return "account/login";
+    }
+
+}
